38a570fc43fb3e67f35158e5969590389134ad15
Rudy Lattae <rudylattae@gmail.com>
2011-11-03 00:20:40 -0600

    Cleanup for pep8 compliance

--- bottle_servefiles.py
+++ bottle_servefiles.py
@@ -15,48 +15,50 @@ class StaticFileRepository:
         if not os.path.exists(config['ROOT_DIR']):
             raise Exception("Crap")
         self.root_dir = config['ROOT_DIR']
-    
+
     def get(self, filename):
         return static_file(filename, root=self.root_dir)
-        
+
 
 def create_app(custom_config=None, app=None):
-    """App factory. Builds an instance of the app 
+    """App factory. Builds an instance of the app
     passing it the config that is provided.
-    Since the app object that is created is scoped only to this 
-    function, it is possible to have multiple instances that do 
+    Since the app object that is created is scoped only to this
+    function, it is possible to have multiple instances that do
     not mess with each other."""
     config = _config.copy()
-    if custom_config: config.update(custom_config)
-    if not app: app = Bottle()
-    
+    if custom_config:
+        config.update(custom_config)
+    if not app:
+        app = Bottle()
+
     repo = StaticFileRepository(config)
-    
+
     @app.get('/:filename#.+#')
     def get(filename):
-        """Locally scoped proxy for the actual repo action we 
-        wish to call. This evil hack is needed because of functools' 
-        poor implementation of update_wrapper. 
-        See: 
+        """Locally scoped proxy for the actual repo action we
+        wish to call. This is needed because of functools'
+        poor implementation of update_wrapper.
+        See:
          - https://github.com/defnull/bottle/issues/223
-         - http://bugs.python.org/issue3445"""
+         - http://bugs.python.org/issue3445
+         """
         return repo.get(filename)
-        
+
     return app
 
-    
+
 def main(argv):
     if len(sys.argv) > 1:
         root_dir = os.path.abspath(sys.argv[1])
     else:
         root_dir = os.path.abspath('.')
-    
+
     print 'serving files in: ', root_dir
     app = create_app({'ROOT_DIR': root_dir})
     bottle.run(app)
 
-    
+
 if __name__ == '__main__':
     import sys
     main(sys.argv)
-    
